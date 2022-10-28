aa138467d311f501f571f74bccaa19b2005239ba
fahadsidd107 <fsiddiqui107@gmail.com>
2022-10-29 01:20:11 +0500

    work

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,22 +2,22 @@ import Link from 'next/link'
 import React from 'react'
 
 function Header() {
-  return (
-    <div>
+    return (
         <div>
-<Link href='/'>
-    <img className='w-44 object-contain cursor-pointer' src='https://links.papareact.com/yvf'/>
-</Link>
-<div>
-<h3>About</h3>
-<h3>Contact</h3>
-<h3>Follow</h3>
-</div>
-        </div>
+            <div>
+                <Link href='/'>
+                    <img className='w-44 object-contain cursor-pointer' src='https://links.papareact.com/yvf' />
+                </Link>
+                <div>
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3>Follow</h3>
+                </div>
+            </div>
 
-        <div></div>
-    </div>
-  )
+            <div></div>
+        </div>
+    )
 }
 
 export default Header
