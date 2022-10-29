a1cfaaa00108698854032429f6279df3a3939809
fahadsidd107 <fsiddiqui107@gmail.com>
2022-10-30 01:22:31 +0500

    work

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
                 <Link href='/'>
                     <img className='w-44 object-contain cursor-pointer' src='https://links.papareact.com/yvf' />
                 </Link>
-                <div>
+                <div className='hidden md:inline-flex'>
                     <h3>About</h3>
                     <h3>Contact</h3>
                     <h3>Follow</h3>
