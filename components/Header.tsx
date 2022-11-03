bf3ec1f66a176f44222b76bb50577148bf5c3d43
fahadsidd107 <fsiddiqui107@gmail.com>
2022-11-04 02:04:42 +0500

    work

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <header className='flex justify-between p-5 max-w-7xl'>
+        <header className='flex justify-between p-5 max-w-7xl mx-auto'>
             <div className='flex items-center space-x-5'>
                 <Link href='/'>
                     <img className='w-44 object-contain cursor-pointer' src='https://links.papareact.com/yvf' />
