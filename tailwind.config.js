35213ce7334b94fe71eac571ad6642a09b501660
fahadsidd107 <fsiddiqui107@gmail.com>
2022-11-08 02:16:44 +0500

    work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},  
+    fontFamily: {
+      custom1: ["Custom-1", "sans-serif"],
+      custom2: ["Custom-2", "sans-serif"],
+    },
+
+    extend: {},
   },
   plugins: [],
-}
+};
