c19be9137e6e1173b51032bbd0c516be0ad6bb1e
fahadsidd107 <fsiddiqui107@gmail.com>
2022-11-09 02:18:31 +0500

    work

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      custom1: ["Custom-1", "sans-serif"],
-      custom2: ["Custom-2", "sans-serif"],
+      serif: ["serif", "serif"],
+      
     },
 
     extend: {},
