76b233e53001a9879f35cf8c47b31d8392a6eafd
fahadsidd107 <fsiddiqui107@gmail.com>
2022-11-21 23:21:02 +0500

    work

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -19,60 +19,60 @@ function Post({post}:Props) {
 
 export default Post;
 
-// export const getSaticPaths= async () => {
-//   const qeury = `*[_type=="post"]{
-//   _id,
-//   slug{
-//   current
-// }
-// }`;
+export const getSaticPaths= async () => {
+  const qeury = `*[_type=="post"]{
+  _id,
+  slug{
+  current
+}
+}`;
 
-//   const posts = await sanityClient.fetch(qeury);
+  const posts = await sanityClient.fetch(qeury);
 
-//   const paths = posts.map((post: Post) => ({
-//     params: {
-//       slug: post.slug.current,
-//     },
-//   }));
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
 
-//   return {
-//     paths,
-//     fallback: "blocking",
-//   };
-// };
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
 
 
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const qeury = `*[_type=="post" && slug.current == "$slug"][0]{
-//   _id,
-//   _createdAt,
-//   author->{
-//   name,
-//   image
-// },
-// description,
-// mainImage,
-// slug,
-// body
-// }`;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const qeury = `*[_type=="post" && slug.current == "$slug"][0]{
+  _id,
+  _createdAt,
+  author->{
+  name,
+  image
+},
+description,
+mainImage,
+slug,
+body
+}`;
 
-//   const posts = await sanityClient.fetch(qeury, {
-//     slug: params?.slug,
-//   });
+  const posts = await sanityClient.fetch(qeury, {
+    slug: params?.slug,
+  });
 
-//   if (!posts) {
-//     return {
-//       notFound: true,
-//     };
-//   }
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
 
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// };
+  return {
+    props: {
+      posts,
+    },
+  };
+};
 
 
 
