4cd459a9dc9165dd094a788673bb69618062300c
fahadsidd107 <fsiddiqui107@gmail.com>
2022-11-30 01:42:54 +0500

    work

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Logo from "../assets/medium.png";
 import { sanityClient,urlFor } from "../sanity";
 
-export const Home: NextPage = () => {
+export const Home: NextPage = (props:Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
