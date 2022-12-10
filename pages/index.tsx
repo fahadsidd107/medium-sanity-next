63f0f418126103dcb9deafbbbf0f5008c4950075
fahadsidd107 <fsiddiqui107@gmail.com>
2022-12-11 02:42:57 +0500

    work

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,9 +48,9 @@ const Home: any = ({ posts }: Props) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:pd-6">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="w-full object-cover">
+            <div className="group">
               {post.mainImage && (
-                <img src={urlFor(post.mainImage).url()} alt={post.title} />
+                <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()} alt={post.title} />
               )}
             </div>
             <div className="flex justify-between p-5 bg-white">
