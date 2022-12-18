7166a1c64e787a02f5984392a5de304c9da3cc6a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-12-18 21:40:11 +0500

    work

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { sanityClient, urlFor } from "../../sanity";
 import Header from "../../components/Header";
+import { Post } from "../../typings";
 function Post() {
   return (
     <main>
@@ -19,6 +20,16 @@ export const getSaticPaths = async () => {
 }
 }`;
 
-const posts = await sanityClient.fetch(qeury);
+  const posts = await sanityClient.fetch(qeury);
 
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+return {
+  paths,
+  fallback:"blocking",
+}
 };
