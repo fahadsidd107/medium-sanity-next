7153395916c8aa59c3b71f63410506a7db75bff0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-12-18 21:33:19 +0500

    work

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { sanityClient,urlFor } from '../../sanity'
-import Header from '../../components/Header'
+import React from "react";
+import { sanityClient, urlFor } from "../../sanity";
+import Header from "../../components/Header";
 function Post() {
   return (
     <main>
-<Header/>
+      <Header />
     </main>
-  )
+  );
 }
 
-export default Post
+export default Post;
 
-export const getSaticPaths = async () =>{
-const qeury = ``
+export const getSaticPaths = async () => {
+  const qeury = `*[_type=="post"]{
+  _id,
+  slug{
+  current
 }
+}`;
+
+const posts = await sanityClient.fetch(qeury);
+
+};
