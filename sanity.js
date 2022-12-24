84d2064e62e0da3caa28a08f013b1157d61c812d
fahadsidd107 <fsiddiqui107@gmail.com>
2022-12-24 21:56:21 +0500

    work

--- sanity.js
+++ sanity.js
@@ -1,16 +1,20 @@
 import {
-  createImageUrlBuilder,
+  // createImageUrlBuilder,
   createCurrentUserHook,
   createClient,
 } from "next-sanity";
+import createImageUrlBuilder from '@sanity/image-url';
 
+
+console.log(process.env.NEXT_PUBLIC_PROJECT_ID)
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId:'vsjmslk9',
   appVersion: "21-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
 export const urlFor = (source) => createImageUrlBuilder(config).image(source);
-export const useCurrentUser = createCurrentUserHook(config);
+// export const useCurrentUser = createCurrentUserHook(config);
