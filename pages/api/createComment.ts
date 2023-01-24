03a676d3f6ce298f5cd7140156e3199304ec75fd
fahadsidd107 <fsiddiqui107@gmail.com>
2023-01-25 02:08:52 +0500

    config

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,6 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+ const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "vsjmslk9",
+    useCdn: process.env.NODE_ENV === "production",
+    token : process.env.NEXT_PUBLIC_SANITY_API_TOKEN || ""
+  };
+
 type Data = {
   name: string;
 };
--- sanity.js
+++ sanity.js
@@ -8,7 +8,6 @@ import imageUrlBuilder from "@sanity/image-url";
 console.log(process.env.NEXT_PUBLIC_PROJECT_ID);
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  // projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   projectId: "vsjmslk9",
   appVersion: "21-03-25",
   useCdn: process.env.NODE_ENV === "production",
