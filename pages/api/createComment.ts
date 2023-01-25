c3cb2bd6fd58c6bc7e7b6bda57aecc983a0247ef
fahadsidd107 <fsiddiqui107@gmail.com>
2023-01-26 04:04:09 +0500

    spi

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,23 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import  { createClient }   from "@sanity/client";
+import { createClient } from "@sanity/client";
 
- const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "vsjmslk9",
-    useCdn: process.env.NODE_ENV === "production",
-    token : process.env.NEXT_PUBLIC_SANITY_API_TOKEN || "skPMdMRT0Fru6tr4qkNCsckFmTAqMIXjBHyMi8jGMLFwCkK6MtifwLaJ283HXhboGlZx8cW1GFdKTC3hIRF54jtyOH2mvZBwYV3DXWIVLs2f3LIREQk2K68j0COQOPE9icc8fB1Ir261FUyIAgO8OZoggw9UGGN6yOTclEtOh3Ua5y2efpCo"
-  };
-
-type Data = {
-  name: string;
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "vsjmslk9",
+  useCdn: process.env.NODE_ENV === "production",
+  token:
+    process.env.NEXT_PUBLIC_SANITY_API_TOKEN ||
+    "skPMdMRT0Fru6tr4qkNCsckFmTAqMIXjBHyMi8jGMLFwCkK6MtifwLaJ283HXhboGlZx8cW1GFdKTC3hIRF54jtyOH2mvZBwYV3DXWIVLs2f3LIREQk2K68j0COQOPE9icc8fB1Ir261FUyIAgO8OZoggw9UGGN6yOTclEtOh3Ua5y2efpCo",
 };
 
-const client= createClient(config)
+type Data = {
+  message: string;
+};
 
-export default function createComment(
+const client = createClient(config);
+
+export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name:"Muhammad Fahad Siddiqui" });
+  const { _id, name, email, comment } = JSON.parse(req.body);
+  try {
+    await client.create({
+      _type: "comment",
+      post: {
+        _type: "reference",
+        _ref: _id,
+      },
+      name,
+      email,
+      comment,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "Unable to create comment." });
+  }
+  console.log("Comment created");
+  return res.status(200).json({ message: "Comment Submitted " });
 }
